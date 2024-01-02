dd51cb94869761315237982e4f993138ba1cf700
Sarkhan <abdullayevsarxan92@gmail.com>
2024-01-03 00:44:57 +0400

    feat: correcting style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
-import logo from '../../resources/img/logo.png';
+import logo from '../../resources/img/roundLogo.png';
 import './header.scss';
 
 function Header() {
@@ -35,83 +35,80 @@ function Header() {
         <p className="brand-text">Əməyin mühafizəsinin idarəetməsində tərəfdaşınız</p>
          </Navbar.Brand>
        
-        <Navbar.Collapse id="basic-navbar-nav">
-
-
-          <Nav className="me-auto">
-            <Nav.Link href="/" >Əsas səhifə</Nav.Link>
-            <NavDropdown
-             title="Haqqımızda" 
-             id="basic-nav-dropdown"
-             show={showDropdown1}
-             onMouseEnter={() => handleMouseEnter(showDropdown1, setShowDropdown1, dropdownRef1)}
-             onMouseLeave={() => handleMouseLeave(showDropdown1, setShowDropdown1, dropdownRef1)}
-           >
-              <NavDropdown.Item href="/about">Haqqımızda məlumat</NavDropdown.Item>
-              <NavDropdown.Item href="/contacts">
-                Bizimlə əlaqə
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-            </NavDropdown>
-
-            <NavDropdown title="Xidmətlərimiz" id="basic-nav-dropdown"
-            show={showDropdown2}
-            onMouseEnter={() => handleMouseEnter(showDropdown2, setShowDropdown2, dropdownRef2)}
-            onMouseLeave={() => handleMouseLeave(showDropdown2, setShowDropdown2, dropdownRef2)}
-          >
-              <NavDropdown.Item href="/beginner">
-                Əməyin mühafizəsi üzrə yeni başlayan işçilərə konsultasiya 
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/documents">
-                Sənədlərin hazırlanması
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/system">
-                SƏTƏMM İdarəetmə Sisteminin qurulması
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/others">
-                 Digər xidmətlər
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-            </NavDropdown>
-
-            <NavDropdown title="Materiallar" id="basic-nav-dropdown"
-            show={showDropdown3}
-            onMouseEnter={() => handleMouseEnter(showDropdown3, setShowDropdown3, dropdownRef3)}
-            onMouseLeave={() => handleMouseLeave(showDropdown3, setShowDropdown3, dropdownRef3)}
-          >
-                <NavDropdown.Item href="/safety">
-                   Əməyin mühafizəsi
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/fire">
-                  Yanğın təhlükəsizliyi
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/firstaid">
-                  İlk tibbi yardım
+        <Navbar.Collapse id="basic-navbar-nav"  className="me-auto">
+          <Nav>
+            <div class="nav-item"><Nav.Link href="/" >Əsas səhifə</Nav.Link></div>
+              <NavDropdown
+              title="Haqqımızda" 
+              id="basic-nav-dropdown"
+              show={showDropdown1}
+              onMouseEnter={() => handleMouseEnter(showDropdown1, setShowDropdown1, dropdownRef1)}
+              onMouseLeave={() => handleMouseLeave(showDropdown1, setShowDropdown1, dropdownRef1)}
+            >
+                <NavDropdown.Item href="/about">Haqqımızda məlumat</NavDropdown.Item>
+                <NavDropdown.Item href="/contacts">
+                  Bizimlə əlaqə
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-            </NavDropdown>
+              </NavDropdown>
 
-            <NavDropdown title="Testlər" id="basic-nav-dropdown"
-            
-            show={showDropdown4}
-            onMouseEnter={() => handleMouseEnter(showDropdown4, setShowDropdown4, dropdownRef4)}
-            onMouseLeave={() => handleMouseLeave(showDropdown4, setShowDropdown4, dropdownRef4)}
-          >
-                <NavDropdown.Item href="/safety">Əməyin mühafizəsi üzrə testlər
+              <NavDropdown title="Xidmətlərimiz" id="basic-nav-dropdown"
+              show={showDropdown2}
+              onMouseEnter={() => handleMouseEnter(showDropdown2, setShowDropdown2, dropdownRef2)}
+              onMouseLeave={() => handleMouseLeave(showDropdown2, setShowDropdown2, dropdownRef2)}
+            >
+                <NavDropdown.Item href="/beginner">
+                  Əməyin mühafizəsi üzrə yeni başlayan işçilərə konsultasiya 
                 </NavDropdown.Item>
-                <NavDropdown.Item href="/fire">
-                  Yanğın təhlükəsizliyi üzrə testlər
+                <NavDropdown.Item href="/documents">
+                  Sənədlərin hazırlanması
                 </NavDropdown.Item>
-                <NavDropdown.Item href="/firstaid">
-                  İlk tibbi yardım üzrə testlər
+                <NavDropdown.Item href="/system">
+                  SƏTƏMM İdarəetmə Sisteminin qurulması
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/others">
+                  Digər xidmətlər
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-            </NavDropdown>
+              </NavDropdown>
+
+              <NavDropdown title="Materiallar" id="basic-nav-dropdown"
+              show={showDropdown3}
+              onMouseEnter={() => handleMouseEnter(showDropdown3, setShowDropdown3, dropdownRef3)}
+              onMouseLeave={() => handleMouseLeave(showDropdown3, setShowDropdown3, dropdownRef3)}
+            >
+                  <NavDropdown.Item href="/safety">
+                    Əməyin mühafizəsi
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/fire">
+                    Yanğın təhlükəsizliyi
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/firstaid">
+                    İlk tibbi yardım
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+              </NavDropdown>
+
+              <NavDropdown title="Testlər" id="basic-nav-dropdown"
+              
+              show={showDropdown4}
+              onMouseEnter={() => handleMouseEnter(showDropdown4, setShowDropdown4, dropdownRef4)}
+              onMouseLeave={() => handleMouseLeave(showDropdown4, setShowDropdown4, dropdownRef4)}
+            >
+                  <NavDropdown.Item href="/safety">Əməyin mühafizəsi üzrə testlər
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/fire">
+                    Yanğın təhlükəsizliyi üzrə testlər
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/firstaid">
+                    İlk tibbi yardım üzrə testlər
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+              </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    
   );
 }
 
